00249ed858e4663cc6d82d4e5d1f7156654ef4b2
Snigdha Kodavasal <skodavasal@gmail.com>
2026-01-02 17:46:27 -0600

    Update DatabaseManager.java

--- db/DatabaseManager.java
+++ db/DatabaseManager.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    // REPLACE THIS with the actual path from your Mac (pwd in terminal)
     private static final String ACTUAL_PATH = "/Users/snigdhakodavasal/CodingFun/APITesting/banking.db";
     private static final String URL = "jdbc:sqlite:" + ACTUAL_PATH;
 
